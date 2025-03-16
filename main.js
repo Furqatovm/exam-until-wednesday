5ca834258c31ff5e3185136d850dfefca7ab9d98
Furqatovm <muhammadyusuffurqatov91@gmail.com>
2025-03-16 18:02:10 +0500

    add hamburher line menu

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-// let nav =document.getElementById("wrapnav");
+let nav =document.getElementById("wrapnav");
 
 
-// let openNav =document.getElementById("openn")
+let openNav =document.getElementById("openn")
 
-// let closeNav =document.getElementById("closess");
+let closeNav =document.getElementById("closess");
 
-// openNav.addEventListener("click", function(){
-//     openNav.style.display ="none";
-//     openNav.style.transition ="500ms ease";
-//     nav.style.display ="block";
-//     nav.style.position ="fixed";
-// })
+openNav.addEventListener("click", function(){
+    openNav.style.display ="none";
+    nav.style.display ="flex";
+
+    let login =document.createElement("a");
+
+    let loginTugma =document.createElement("button");
+
+    login.appendChild(loginTugma);
+    nav.appendChild(login);
+
+    loginTugma.className ="login-btn";
+    loginTugma.textContent ="login";
+    closeNav.style.display ="block";
+
+})
+
+closeNav.addEventListener("click", function(){
+    nav.style.display ="none";
+    closeNav.style.display ="none";
+    openNav.style.display ="block";
+})

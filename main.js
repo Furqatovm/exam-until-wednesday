07ed885b8f2d8e2cb62851621d0eb54d2be8329c
Furqatovm <muhammadyusuffurqatov91@gmail.com>
2025-03-16 16:54:28 +0500

    make responsive for 500px phones

---
+++ main.js
@@ -0,0 +1,13 @@
+// let nav =document.getElementById("wrapnav");
+
+
+// let openNav =document.getElementById("openn")
+
+// let closeNav =document.getElementById("closess");
+
+// openNav.addEventListener("click", function(){
+//     openNav.style.display ="none";
+//     openNav.style.transition ="500ms ease";
+//     nav.style.display ="block";
+//     nav.style.position ="fixed";
+// })

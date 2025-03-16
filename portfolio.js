1a86a6068e7f37aa6e2b68dfe79ebf6050db3bf7
Furqatovm <muhammadyusuffurqatov91@gmail.com>
2025-03-16 22:03:46 +0500

    make responsive

---
+++ portfolio.js
@@ -0,0 +1,31 @@
+
+let nav =document.getElementById("wrapnav");
+
+
+let openNav =document.getElementById("openn")
+
+let closeNav =document.getElementById("closess");
+
+openNav.addEventListener("click", function(){
+    openNav.style.display ="none";
+    nav.style.display ="flex";
+
+    if(!document.querySelector(".login-btn")){
+        let login =document.createElement("a");
+        let loginTugma =document.createElement("button");
+        login.appendChild(loginTugma)
+        loginTugma.className ="login-btn";
+        loginTugma.textContent ="login";
+        nav.appendChild(login)
+    }
+
+
+    closeNav.style.display ="block";
+
+})
+
+closeNav.addEventListener("click", function(){
+    nav.style.display ="none";
+    closeNav.style.display ="none";
+    openNav.style.display ="block";
+})
